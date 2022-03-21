6e68da9101d914009326fa0c0f476cce24527457
Prashant Singh <prashant02ps@gmail.com>
2022-03-21 16:03:43 +0530

    registeration done

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField, BooleanField
-from wtforms.validators import DataRequired,Length,EqualTo, Email
+from wtforms.validators import DataRequired,Length,EqualTo, Email,ValidationError
+from flaskblog.models import User
 
 class RegistrationForm(FlaskForm):
     username=StringField('Username',
@@ -11,6 +12,17 @@ class RegistrationForm(FlaskForm):
     confirm_password= PasswordField('Confirm Password',
                         validators=[DataRequired(), EqualTo('password')])
     submit=SubmitField('Sign Up')
+
+    def validate_username(self,username):
+        user=User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('The username is taken. Please take a diffrent name')
+    
+    def validate_email(self,email):
+        user=User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('The email is taken. Please take a diffrent email')
+
 
 class LoginForm(FlaskForm):
     email=StringField('Email',validators=[DataRequired()])
