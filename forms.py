eb54281de01724870b7114b1df7bbd292e3d8abf
Prashant Singh <prashant02ps@gmail.com>
2022-03-21 14:01:23 +0530

    0

--- forms.py
+++ forms.py
@@ -6,14 +6,14 @@ class RegistrationForm(FlaskForm):
     username=StringField('Username',
                         validators=[DataRequired(), Length(min=2,max=20)])
     email=StringField('Email',
-                        validators=[DataRequired(),Email()])
+                        validators=[DataRequired()])
     password= PasswordField('Password',validators=[DataRequired()])
     confirm_password= PasswordField('Confirm Password',
                         validators=[DataRequired(), EqualTo('password')])
     submit=SubmitField('Sign Up')
 
 class LoginForm(FlaskForm):
-    email=StringField('Email',validators=[DataRequired(),Email()])
+    email=StringField('Email',validators=[DataRequired()])
     password= PasswordField('Password',validators=[DataRequired()])
     remember=BooleanField('Remember Me')
     submit=SubmitField('Login')
