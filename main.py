5401bda1894761762cb0ccd6ebc4f68c8da46e52
Prashant Singh <prashant02ps@gmail.com>
2022-03-21 15:05:21 +0530

    database creaqted

--- main.py
+++ main.py
@@ -1,10 +1,39 @@
+from datetime import datetime
+from email.policy import default
 from flask import Flask, redirect , render_template ,url_for,flash,redirect
 from forms import RegistrationForm, LoginForm
+from flask_sqlalchemy import SQLAlchemy
+
 app=Flask(__name__)
 
 app.config['SECRET_KEY'] = 'nvd68a4fc168as46f'
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///site.db'
 
-class Post:
+db=SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    posts = db.relationship("Post", backref='author', lazy=True) 
+
+    def __repr__(self):
+        return f"User('{self.username}','{self.email}','{self.image_file}')"
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}')"
+
+
+class ost():
     def __init__(self,author,title,content,date_posted):
         self.author=author
         self.title=title
@@ -16,8 +45,8 @@ class Post:
         return self.title+" BY "+self.author
 
 list=[]
-list.append(Post('Raman','The Story','the content','02-12-2005'))
-list.append(Post('Simon','Never Say Never','Content','02-10-2000'))
+list.append(ost('Raman','The Story','the content','02-12-2005'))
+list.append(ost('Simon','Never Say Never','Content','02-10-2000'))
 
 @app.route("/")
 @app.route('/home')
