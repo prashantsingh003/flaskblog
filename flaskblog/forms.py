943ae45435c623c2a905cec40c082f7c13022b9c
Prashant Singh <prashant02ps@gmail.com>
2022-04-01 15:09:35 +0530

    a

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -3,12 +3,12 @@ from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
 from flask_login import current_user
 from wtforms import StringField, TextAreaField, SubmitField, PasswordField, BooleanField
-from wtforms.validators import DataRequired,Length,EqualTo, Email,ValidationError
+from wtforms.validators import DataRequired,Length,EqualTo, Email,ValidationError,InputRequired
 from flaskblog.models import User
 
 class RegistrationForm(FlaskForm):
     username=StringField('Username',
-                        validators=[DataRequired(), Length(min=2,max=20)])
+                        validators=[InputRequired(message="Please enter a user name"), Length(min=2,max=20)])
     email=StringField('Email',
                         validators=[DataRequired()])
     password= PasswordField('Password',validators=[DataRequired()])
